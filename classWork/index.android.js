e5db62f4168f19fd316f3e728f0527e06631444b
LitileXueZha <1941639715@qq.com>
2018-01-04 21:32:03 +0800

    hello

--- classWork/index.android.js
+++ classWork/index.android.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import {
   AppRegistry,
 } from 'react-native';
 import StartComponent from './views/StartComponent';
 
-AppRegistry.registerComponent('classWork', () => StartComponent);
+class HelloWord extends Component {
+  render() {
+    console.log('hello word');
+    return (
+      <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Hello Word</Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('classWork', () => HelloWord);
